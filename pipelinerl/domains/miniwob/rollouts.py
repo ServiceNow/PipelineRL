b16d45c6b2496a017aa5e47817e57485b8ed83a2
Nicolas Gontier <gontier.nicolas@yahoo.fr>
2025-08-28 18:39:11 +0000

    revert reward calculation

--- pipelinerl/domains/miniwob/rollouts.py
+++ pipelinerl/domains/miniwob/rollouts.py
@@ -142,12 +142,12 @@ async def generate_miniwob_rollout(
     # get the number of PageObservation steps in the tape
     n_page_observations = len([step for step in tape.steps if isinstance(step, PageObservation)])
 
-    # reward = raw_reward * 0.99**n_step_errors if no_error and raw_reward >= 0 else -1.0
+    reward = raw_reward * 0.99**n_step_errors if no_error and raw_reward >= 0 else -1.0
     # massimo's setup:
-    reward = float(raw_reward>0)
-    if reward == 0.0:
-        reward = -1.0
-    reward *= 0.98 ** n_page_observations
+    # reward = float(raw_reward>0)
+    # if reward == 0.0:
+    #     reward = -1.0
+    # reward *= 0.98 ** n_page_observations
 
     # (3) Get LLM calls from Tape
     llm_calls = [step for step in tape.steps if step.metadata.other.get("llm_call") is not None]
