e6f232911759b43d159eb1742571967819bb8a62
Oleh Shliazhko <oleh.shliazhko@servicenow.com>
2025-10-06 17:51:47 +0000

    update debug entrypoint

--- debug.sh
+++ debug.sh
@@ -1,18 +1,71 @@
 #!/bin/bash
-python -m pipelinerl.launch \
-    output_dir=results/actor_debug1 \
+echo "Run 32 workers"
+DEBUG_FILE=timing_debug_workers32_3.jsonl python -m pipelinerl.launch \
+    output_dir=results/actor_debug32_3 \
     force_restart=true \
-    world.env_replicas_per_actor=1 \
-    actor.llm_max_rollouts=16 \
+    actor.llm_max_rollouts=256 \
     finetune.seq_parallel=8 \
     eval_every_n_versions=0 \
-    actor.rollout_workers=1 \
+    actor.rollout_workers=32 \
     debug.mode=actor \
     world.actor_fraction=8 \
     world.finetune_fraction=0 \
     world.preprocessor_fraction=0 \
     --config-name mcp
 
-    # environment.n_envs=4 \
-    # environment.mcp_read_timeout_seconds=300 \
-    # environment.env_call_timeout=300 \
+# echo "Run 10 workers"
+# DEBUG_FILE=timing_debug_gpt_workers10.jsonl python -m pipelinerl.launch \
+#     output_dir=results/actor_debug2 \
+#     force_restart=true \
+#     actor.llm_max_rollouts=16 \
+#     finetune.seq_parallel=8 \
+#     eval_every_n_versions=0 \
+#     actor.rollout_workers=10 \
+#     debug.mode=actor \
+#     world.actor_fraction=8 \
+#     world.finetune_fraction=0 \
+#     world.preprocessor_fraction=0 \
+#     --config-name mcp
+
+
+# echo "Run 5 workers"
+# DEBUG_FILE=timing_debug_gpt_workers5.jsonl python -m pipelinerl.launch \
+#     output_dir=results/actor_debug2 \
+#     force_restart=true \
+#     actor.llm_max_rollouts=16 \
+#     finetune.seq_parallel=8 \
+#     eval_every_n_versions=0 \
+#     actor.rollout_workers=5 \
+#     debug.mode=actor \
+#     world.actor_fraction=8 \
+#     world.finetune_fraction=0 \
+#     world.preprocessor_fraction=0 \
+#     --config-name mcp
+
+# echo "Run 40 workers"
+# DEBUG_FILE=timing_debug_gpt_workers40.jsonl python -m pipelinerl.launch \
+#     output_dir=results/actor_debug2 \
+#     force_restart=true \
+#     actor.llm_max_rollouts=16 \
+#     finetune.seq_parallel=8 \
+#     eval_every_n_versions=0 \
+#     actor.rollout_workers=40 \
+#     debug.mode=actor \
+#     world.actor_fraction=8 \
+#     world.finetune_fraction=0 \
+#     world.preprocessor_fraction=0 \
+#     --config-name mcp
+
+# echo "Run 30 workers"
+# DEBUG_FILE=timing_debug_gpt_workers30.jsonl python -m pipelinerl.launch \
+#     output_dir=results/actor_debug2 \
+#     force_restart=true \
+#     actor.llm_max_rollouts=16 \
+#     finetune.seq_parallel=8 \
+#     eval_every_n_versions=0 \
+#     actor.rollout_workers=30 \
+#     debug.mode=actor \
+#     world.actor_fraction=8 \
+#     world.finetune_fraction=0 \
+#     world.preprocessor_fraction=0 \
+#     --config-name mcp
