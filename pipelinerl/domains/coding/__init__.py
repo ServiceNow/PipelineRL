6f9c5cccff976cafc6c367197bdb413a9b4bfa2d
rafapi <rafael.pardinas@servicenow.com>
2025-11-16 21:37:12 +0000

    sync coding init

--- pipelinerl/domains/coding/__init__.py
+++ pipelinerl/domains/coding/__init__.py
@@ -1,10 +1,17 @@
 """Coding domain rollouts and dataset utilities."""
 
 from .rollouts import generate_coding_rollout
+from .verifier_api import (
+    CodingSandboxEnvironment,
+    evaluate_coding_prediction,
+    verify_coding_solution_rpc,
+)
 from .dataset import load_problems
 
 __all__ = [
+    "CodingSandboxEnvironment",
+    "evaluate_coding_prediction",
     "generate_coding_rollout",
     "load_problems",
+    "verify_coding_solution_rpc",
 ]
-
