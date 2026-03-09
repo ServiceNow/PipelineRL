b554ade0dac9c772800fad39eaf1b40376996a1c
rafapi <rafael.pardinas@servicenow.com>
2026-03-09 18:59:33 +0000

    add vendored logic verifiers

---
+++ pipelinerl/domains/logic/i3_logic/__init__.py
@@ -0,0 +1,13 @@
+"""Vendored i3_logic package from PrimeIntellect.
+
+Source: https://github.com/PrimeIntellect/i3-logic
+License: Apache 2.0
+
+This package provides verifiers for 87+ logic task types used in the
+INTELLECT-3-RL dataset. Vendored to remove the external dependency.
+"""
+
+from .task2verifier import verifier_classes
+from .base import Data
+
+__all__ = ["verifier_classes", "Data"]
