1fd0e2af4a75e7cef72c84b941c3b244a36b7c70
rafapi <rafael.pardinas@servicenow.com>
2026-07-06 18:36:40 +0000

    rename event credit run to 02 with ec02 fleets

--- qwen35_9b_event_credit.sh
+++ qwen35_9b_event_credit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Phase 1 event-credit run (A_t = z - c|z|(e_t - mean_e)), parallel to the gspo_N mainline.
 # Same 4 nodes x 8 H100 recipe as qwen35_9b_terminal.sh but with
-# --config-name terminal_event_credit (event_credit_coef 0.5 + ec01-suffixed
+# --config-name terminal_event_credit (event_credit_coef 0.5 + ec02-suffixed
 # fleet DNS so it never clashes with the mainline run's fleets).
 
 TIMESTAMP=$(date +%s)
 OUTPUT_DIR_BASE=/mnt/llmd/results/exps/rafa/terminal
-JOB_NAME=${JOB_NAME:-terminal_qwen35_9b_event_credit_01}
+JOB_NAME=${JOB_NAME:-terminal_qwen35_9b_event_credit_02}
 CONDA_ENV=${CONDA_ENV:-pipeline-rl}
 CONDA_EXE=${CONDA_EXE:-/opt/conda/bin/conda}
 
