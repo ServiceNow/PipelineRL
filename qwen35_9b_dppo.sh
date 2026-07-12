8048fc9e08461ba815e2bec88f40c86b73adb331
rafapi <rafael.pardinas@servicenow.com>
2026-07-12 20:07:45 +0000

    add dppo overlay run config

---
+++ qwen35_9b_dppo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# DPPO overlay run: --config-name terminal_dppo (terminal_full_infra + policy_loss
+# dppo, dppo01 fleet DNS). Single variable vs fi03; same 4x8 H100 recipe.
+
+TIMESTAMP=$(date +%s)
+OUTPUT_DIR_BASE=/mnt/llmd/results/exps/rafa/terminal
+JOB_NAME=${JOB_NAME:-terminal_qwen35_9b_dppo_01}
+CONDA_ENV=${CONDA_ENV:-pipeline-rl}
+CONDA_EXE=${CONDA_EXE:-/opt/conda/bin/conda}
+
+COMMAND="python -m pipelinerl.launch \
+output_dir=${OUTPUT_DIR_BASE}/${JOB_NAME} \
+--config-name terminal_dppo \
+--config-dir /home/toolkit/PipelineRL/conf"
+
+make multi-replica-job \
+    REPLICAS=4 \
+    ENV=${CONDA_ENV} \
+    CONDA_EXE=${CONDA_EXE} \
+    SNAPSHOT=0 \
+    NPROC=8 \
+    BID=999 \
+    JOB_NAME=${JOB_NAME}_${TIMESTAMP} \
+    HOME_DATA_NAME="snow.research.tapes.rafael_pardinas_home" \
+    TRANSFORMERS_CACHE_DATA="snow.research.tapes.transformers_cache" \
+    DATA_OBJ="snow.research.tapes.data" \
+    RESULTS_OBJ="snow.research.tapes.results" \
+    BASE_MODELS_OBJ="snow.research.tapes.base_models" \
+    COMMAND="${COMMAND}"
