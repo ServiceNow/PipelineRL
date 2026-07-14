61f03ae2c8793eba8f29b58799282870f1de7dca
rafapi <rafael.pardinas@servicenow.com>
2026-07-14 17:32:49 +0000

    prepare dppo_04 doubled concurrency

--- qwen35_9b_dppo.sh
+++ qwen35_9b_dppo.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# dppo_02: --config-name terminal_dppo03 (terminal_dppo + gradient clip 0.1->1.0,
-# dppo03 fleet DNS). Post-fix baseline; same 4x8 H100 recipe.
+# dppo_04: --config-name terminal_dppo04 (dppo03 + 2x concurrency bundle: llm_max_rollouts 4,
+# dppo04 fleets a-h). Post-fix baseline at speed; same 4x8 H100 recipe.
 
 TIMESTAMP=$(date +%s)
 OUTPUT_DIR_BASE=/mnt/llmd/results/exps/rafa/terminal
-JOB_NAME=${JOB_NAME:-terminal_qwen35_9b_dppo_03}
+JOB_NAME=${JOB_NAME:-terminal_qwen35_9b_dppo_04}
 CONDA_ENV=${CONDA_ENV:-pipeline-rl}
 CONDA_EXE=${CONDA_EXE:-/opt/conda/bin/conda}
 
 COMMAND="python -m pipelinerl.launch \
 output_dir=${OUTPUT_DIR_BASE}/${JOB_NAME} \
---config-name terminal_dppo03 \
+--config-name terminal_dppo04 \
 --config-dir /home/toolkit/PipelineRL/conf"
 
 make multi-replica-job \
