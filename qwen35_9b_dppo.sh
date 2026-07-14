4771dd29acb5932d8f8eced9a5a3a45a60369814
rafapi <rafael.pardinas@servicenow.com>
2026-07-14 12:16:36 +0000

    prepare post-fix dppo baseline

--- qwen35_9b_dppo.sh
+++ qwen35_9b_dppo.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# dppo_02: --config-name terminal_dppo02 (terminal_dppo + gradient clip 0.1->1.0,
-# dppo02 fleet DNS). Single-variable threshold ablation; same 4x8 H100 recipe.
+# dppo_02: --config-name terminal_dppo03 (terminal_dppo + gradient clip 0.1->1.0,
+# dppo03 fleet DNS). Post-fix baseline; same 4x8 H100 recipe.
 
 TIMESTAMP=$(date +%s)
 OUTPUT_DIR_BASE=/mnt/llmd/results/exps/rafa/terminal
-JOB_NAME=${JOB_NAME:-terminal_qwen35_9b_dppo_02}
+JOB_NAME=${JOB_NAME:-terminal_qwen35_9b_dppo_03}
 CONDA_ENV=${CONDA_ENV:-pipeline-rl}
 CONDA_EXE=${CONDA_EXE:-/opt/conda/bin/conda}
 
 COMMAND="python -m pipelinerl.launch \
 output_dir=${OUTPUT_DIR_BASE}/${JOB_NAME} \
---config-name terminal_dppo02 \
+--config-name terminal_dppo03 \
 --config-dir /home/toolkit/PipelineRL/conf"
 
 make multi-replica-job \
