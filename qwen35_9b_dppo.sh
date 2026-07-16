150a8ce7d5a6134cf28ef668dc4de753e172eecd
rafapi <rafael.pardinas@servicenow.com>
2026-07-16 19:45:36 +0000

    prepare dppo_05 no-submit-penalty arm

--- qwen35_9b_dppo.sh
+++ qwen35_9b_dppo.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# dppo_04: --config-name terminal_dppo04 (dppo03 + 2x concurrency bundle: llm_max_rollouts 4,
-# dppo04 fleets a-h). Post-fix baseline at speed; same 4x8 H100 recipe.
+# dppo_05 ARM1: --config-name terminal_dppo05 (dppo04 + no_submit_penalty 0).
+# dppo05 fleets a-h. Single-variable shaping-removal arm; same 4x8 H100 recipe.
 
 TIMESTAMP=$(date +%s)
 OUTPUT_DIR_BASE=/mnt/llmd/results/exps/rafa/terminal
-JOB_NAME=${JOB_NAME:-terminal_qwen35_9b_dppo_04}
+JOB_NAME=${JOB_NAME:-terminal_qwen35_9b_dppo_05}
 CONDA_ENV=${CONDA_ENV:-pipeline-rl}
 CONDA_EXE=${CONDA_EXE:-/opt/conda/bin/conda}
 
 COMMAND="python -m pipelinerl.launch \
 output_dir=${OUTPUT_DIR_BASE}/${JOB_NAME} \
---config-name terminal_dppo04 \
+--config-name terminal_dppo05 \
 --config-dir /home/toolkit/PipelineRL/conf"
 
 make multi-replica-job \
